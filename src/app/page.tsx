14a040025474ef989712fcb6ce4116964bdf661c
israel mvono <israelmvono@gmail.com>
2025-02-22 08:35:06 +0300

    feat: add all sections to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Hero from '@/components/sections/home/Hero';
 import Services from '@/components/sections/home/Services';
+import About from '@/components/sections/home/About';
+import Projects from '@/components/sections/home/Projects';
+import Contact from '@/components/sections/home/Contact';
+import Footer from '@/components/footer/Footer';
 
 export default function Page() {
   return (
-    <main className="relative z-10">
-      <Hero />
-      <Services />
-    </main>
+    <>
+      <main className="relative z-10">
+        <Hero />
+        <Services />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
