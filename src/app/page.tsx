a3c9dd43daca2e8d30250c612f87d00c27e4f897
israel mvono <israelmvono@gmail.com>
2025-02-22 13:57:54 +0300

    fix: update main page with client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Hero from '@/components/sections/home/Hero';
 import Services from '@/components/sections/home/Services';
 import About from '@/components/sections/home/About';
@@ -5,7 +7,7 @@ import Projects from '@/components/sections/home/Projects';
 import Contact from '@/components/sections/home/Contact';
 import Footer from '@/components/footer/Footer';
 
-export default function Page() {
+export default function Home() {
   return (
     <>
       <main className="relative z-10">
