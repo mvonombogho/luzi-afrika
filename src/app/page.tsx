f63a720396a4d088350f19ee14f32a7118f2111e
israel mvono <israelmvono@gmail.com>
2025-02-23 15:40:28 +0300

    fix: update imports to use default imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 // File: src/app/page.tsx
 'use client';
 
-import { Hero } from '@/components/sections/home/Hero';
-import { Services } from '@/components/sections/home/Services';
-import { About } from '@/components/sections/home/About';
-import { Projects } from '@/components/sections/home/Projects';
-import { Contact } from '@/components/sections/home/Contact';
+import Hero from '@/components/sections/home/Hero';
+import Services from '@/components/sections/home/Services';
+import About from '@/components/sections/home/About';
+import Projects from '@/components/sections/home/Projects';
+import Contact from '@/components/sections/home/Contact';
 import Footer from '@/components/footer/Footer';
 
 export default function Home() {
