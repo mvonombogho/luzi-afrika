61d86b5a04102be488815e1b1b8badf397b281c0
israel mvono <israelmvono@gmail.com>
2025-02-23 19:49:56 +0300

    update: add Footer to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,19 @@ import Services from '@/components/sections/home/Services';
 import About from '@/components/sections/home/About';
 import Projects from '@/components/sections/home/Projects';
 import Contact from '@/components/sections/home/Contact';
+import Footer from '@/components/footer/Footer';
 
 export default function Home() {
   return (
-    <main className="relative z-10">
-      <Hero />
-      <Services />
-      <About />
-      <Projects />
-      <Contact />
-    </main>
+    <>
+      <main className="relative z-10">
+        <Hero />
+        <Services />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
