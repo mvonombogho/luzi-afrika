28d959ba1f65af5072ca7a78eecaf9bab213767a
israel mvono <israelmvono@gmail.com>
2025-02-23 16:59:51 +0300

    feat: add back Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import Hero from '@/components/sections/home/Hero';
 import Test from '@/components/Test';
 
 export default function Home() {
   return (
-    <main>
-      <Test />
+    <main className="relative z-10">
+      <Hero />
     </main>
   );
 }
