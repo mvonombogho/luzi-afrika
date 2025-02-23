68ce134b622fc6501852eccece7b61eb9e0dfa2b
israel mvono <israelmvono@gmail.com>
2025-02-23 16:42:49 +0300

    fix: simplify page to debug Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
-// File: src/app/page.tsx
 'use client';
 
-import Hero from '../components/sections/home/Hero';
-import Services from '../components/sections/home/Services';
-import About from '../components/sections/home/About';
-import Projects from '../components/sections/home/Projects';
-import Contact from '../components/sections/home/Contact';
-import Footer from '../components/footer/Footer';
+import Hero from '@/components/sections/home/Hero';
 
 export default function Home() {
   return (
-    <>
-      <main className="relative z-10">
-        <Hero />
-        <Services />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </>
+    <main className="relative z-10">
+      <Hero />
+    </main>
   );
 }
