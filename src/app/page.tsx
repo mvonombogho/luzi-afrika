00dd086f80114d4dea4dd2ade2b223c3e9ba8913
israel mvono <israelmvono@gmail.com>
2025-02-23 16:44:05 +0300

    fix: test basic component import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import Hero from '../components/sections/home/Hero';
+import Test from '@/components/Test';
 
 export default function Home() {
   return (
-    <main className="relative z-10">
-      <Hero />
+    <main>
+      <Test />
     </main>
   );
 }
