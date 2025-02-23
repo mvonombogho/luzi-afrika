c6b1b644b026d5c6d5891a9483c8fe970a20fd08
israel mvono <israelmvono@gmail.com>
2025-02-23 20:26:56 +0300

    Add SEO metadata configuration

---
+++ src/app/metadata.ts
@@ -0,0 +1,57 @@
+import type { Metadata } from 'next';
+
+const siteConfig = {
+  name: 'Luzi Afrika',
+  description: 'Illuminating Africa\'s Digital Future through innovative IT solutions',
+  url: 'https://luziafrika.com',
+  ogImage: 'https://luziafrika.com/og.jpg',
+};
+
+export const baseMetadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@luziafrika',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+};
