421e121eef91cdf153d4144326e99de23708fe5d
israel mvono <israelmvono@gmail.com>
2025-02-23 16:10:23 +0300

    fix: update page to use barrel imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // File: src/app/page.tsx
 'use client';
 
-import Hero from '@/components/sections/home/Hero';
-import Services from '@/components/sections/home/Services';
-import About from '@/components/sections/home/About';
-import Projects from '@/components/sections/home/Projects';
-import Contact from '@/components/sections/home/Contact';
-import Footer from '@/components/footer/Footer';
+import {
+  Hero,
+  Services,
+  About,
+  Projects,
+  Contact,
+  Footer
+} from '@/components';
 
 export default function Home() {
   return (
