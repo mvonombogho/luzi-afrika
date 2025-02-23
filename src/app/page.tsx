356c1d6525786a34b4c8f9d8609d70e61bf1e51f
israel mvono <israelmvono@gmail.com>
2025-02-23 18:39:44 +0300

    update: add About section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Hero from '@/components/sections/home/Hero';
 import Services from '@/components/sections/home/Services';
+import About from '@/components/sections/home/About';
 
 export default function Home() {
   return (
     <main className="relative z-10">
       <Hero />
       <Services />
+      <About />
     </main>
   );
 }
