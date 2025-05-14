d46a0c60e9a487e752331f59a3925bd3d47d3bd9
israel mvono <israelmvono@gmail.com>
2025-05-14 17:00:14 +0300

    Update next.config.js to ignore TypeScript errors during build

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
       },
     ],
   },
+  // For production builds - ignore TypeScript errors
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // Also ignore ESLint errors
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
